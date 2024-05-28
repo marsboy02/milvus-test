6278a515ff7c55be86408a25bfc0fcdd8185722d
Marsboy02 <marsboy0619@gmail.com>
2024-05-28 21:32:06 +0900

    feat: milvus 데이터베이스에서 하이브리드 쿼리에 대한 코드 추가

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pymilvus import (
     Collection,
 )
 import random
+import time
 
 connections.connect("default", host="localhost", port="19530")
 
@@ -34,6 +35,7 @@ entities = [
 insert_result = hello_milvus.insert(entities)
 hello_milvus.flush()
 
+# Index
 print(format("Start Creating index IVF_FLAT"))
 index = {
     "index_type": "IVF_FLAT",
@@ -43,6 +45,9 @@ index = {
 
 hello_milvus.create_index("embeddings", index)
 
+
+# Index Loading & Similarity Search
+'''
 hello_milvus.load()
 vectors_to_search = entities[-1][-2:]
 search_params = {
@@ -54,3 +59,44 @@ result = hello_milvus.search(vectors_to_search, "embeddings", search_params, lim
 for hits in result:
     for hit in hits:
         print(f"hit: {hit}, random field: {hit.entity.get('random')}")
+'''
+
+
+# Filtering & Query
+print(format("Start querying with `random > 0.5`"))
+
+start_time = time.time()
+result = hello_milvus.query(expr="random > 0.5", output_fields=["random", "embeddings"])
+end_time = time.time()
+
+print(f"query result:\n-{result[0]}")
+print(format(end_time - start_time))
+
+
+# Pagination
+r1 = hello_milvus.query(expr="random > 0.5", limit=4, output_fields=["random"])
+r2 = hello_milvus.query(
+    expr="random > 0.5", offset=1, limit=3, output_fields=["random"]
+)
+print(f"query pagination(limit=4):\n\t{r1}")
+print(f"query pagination(offset=1, limit=3):\n\t{r2}") 
+
+
+# hybrid search
+print(format("Start hybrid searching with `random > 0.5`"))
+
+start_time = time.time()
+result = hello_milvus.search(
+    vectors_to_search,
+    "embeddings",
+    search_params,
+    limit=3,
+    expr="random > 0.5",
+    output_fields=["random"],
+)
+end_time = time.time()
+
+for hits in result:
+    for hit in hits:
+        print(f"hit: {hit}, random field: {hit.entity.get('random')}")
+print(search_latency_fmt.format(end_time - start_time))
