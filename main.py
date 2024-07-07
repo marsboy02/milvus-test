e70509bd37a02ecca6eea463edc023c58027e62b
Marsboy02 <marsboy0619@gmail.com>
2024-07-07 20:13:13 +0900

    feat: embedding_list를 반환하는 코드 추가

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# milvus.py
 from model.embedder import TabularEmbedder
+
 
 def main():
     dataset_dir = "./dataset/"
     embedder = TabularEmbedder(dataset_dir=dataset_dir)
     embeddings_list = embedder.process_files()
 
-    # for embedding, column_names in embeddings_list:
-    #     print(f"Embedding: {embedding}")
-    #     print(f"Column Names: {column_names}")
-    print(embeddings_list)
+    for embedding, column_names in embeddings_list:
+        print(f"Embedding: {embedding}")
+        print(f"Column Names: {column_names}")
+
 
 if __name__ == "__main__":
     main()
